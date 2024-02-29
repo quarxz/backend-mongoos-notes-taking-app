6365cfbe45c86976d6fc4de87aa203e2bf2fef10
Falk König <fk@falkkoenig.de>
2024-02-29 12:18:02 +0100

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors"); // Alle Browser haben Cross Origin Zugriff
 const port = process.env.PORT || 3000;
 app.use(express.json());
 const connect = require("./lib/connect");
