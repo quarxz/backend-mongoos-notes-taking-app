1634e8c5d06802fce0a1527d7a4124bb06d6ea00
Falk König <fk@falkkoenig.de>
2024-03-01 10:38:53 +0100

    show notes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const connect = require("../lib/connect");
 const Note = require("../models/Note");
+const User = require("../models/User");
 
 const users = require("./users.route");
 
@@ -27,13 +28,21 @@ r.use("/:user", users);
 
 r.get("/", async (req, res) => {
   await connect();
+
+  const users = await User.find();
+  if (!users.length) {
+    return res.status(404).json({ message: "Users not found" });
+  }
+
   const notes = await Note.find().populate("user", "name");
 
   if (!notes.length) {
     return res.status(404).json({ message: "note not found" });
   }
+  console.log(notes);
+  return res.json(notes);
 
-  return res.status(200).json(notes.map((note) => ({ ...note._doc, id: note._id })));
+  // return res.status(200).json(notes.map((note) => ({ ...note._doc, id: note._id })));
 });
 
 module.exports = r;
